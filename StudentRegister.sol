fdcd6322552a03bb3ea6f07050b2775b5b215c1e
LucianoSainz <danielsainz28@gmail.com>
2026-09-12 16:46:12 +0000

    Add function getTotalStudents

--- StudentRegister.sol
+++ StudentRegister.sol
@@ -1,8 +1,20 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Student {
-    sitring name;
-    uint256 note
+contract StudentRegistration {
+
+    struct Student {
+        string name;
+        uint256 note;
+    }
+
+    Student[] public listOfStudents;
+
+    function addStudent(string memory _nombre, uint256 _nota) public {
+        listOfStudents.push(Student(_nombre, _nota));
+    }
+
+    function getTotalStudents() public view returns (uint256) {
+        return listOfStudents.length;
+    }
 }
